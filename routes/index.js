51d2d2e00e84aae5f0fda1465d7ec352add0ae7b
Mohammed <sajidcurious9@gmail.com>
2024-02-05 15:08:34 +0530

    passed values to the schema

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index");
+router.get("/create", async function (req, res, next) {
+  const createdUser = await userModel.create({
+    username: "Sajid",
+    name: "Sajid",
+    age: 23,
+  });
+  res.send(createdUser);
 });
 
 module.exports = router;
