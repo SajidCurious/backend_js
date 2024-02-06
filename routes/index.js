9dd057ddd7e5c42b3fcb31c4e6b6a996de2db751
Mohammed <sajidcurious9@gmail.com>
2024-02-06 14:26:53 +0530

    installed session package

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,23 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 
-/* GET home page. */
-router.get("/create", async function (req, res, next) {
-  const createdUser = await userModel.create({
-    username: "Sajid",
-    name: "Sajid",
-    age: 23,
-  });
-  res.send(createdUser);
+// router.get("/create", async function (req, res, next) {
+//   const createdUser = await userModel.create({
+//     username: "Sajid",
+//     name: "Sajid",
+//     age: 23,
+//   });
+//   res.send(createdUser);
+// });
+
+// router.get("/users", async function (req, res, next) {
+//   const findUser = await userModel.find();
+//   res.send(findUser);
+// });
+
+router.get("/delete", async function (req, res, next) {
+  const deleteUser = await userModel.findOneAndDelete();
+  res.send(deleteUser);
 });
 
 module.exports = router;
